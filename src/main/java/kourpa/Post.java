2888fdb380cd3b1c89a5607b5571f7caa697e53d
Dimitris Doukas <t8210038@aueb.gr>
2022-12-04 14:52:27 +0200

    Add Upload and Post class

---
+++ src/main/java/kourpa/Post.java
@@ -0,0 +1,16 @@
+package kourpa;
+import java.util.HashMap;
+
+public class Post {
+	public HashMap<String, HashMap<String, String>> storeMessage(String tip, String categ) {
+		HashMap<String, HashMap<String, String>> outer = new HashMap<>();
+		HashMap<String, String> inner = new HashMap<>();
+		inner.put(categ, tip);
+		outer.put("dim", inner);
+		System.out.println(outer);
+		return outer;
+	}
+	 public static void main(String[] args) {
+		Upload upload = new Upload();
+	}
+}
