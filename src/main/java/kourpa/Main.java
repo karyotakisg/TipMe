5c0676aab4e7488eb5e6c87ca60bc937d31a13c1
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2022-12-07 11:05:46 +0200

    SQLite dependency added

--- src/main/java/kourpa/Main.java
+++ src/main/java/kourpa/Main.java
@@ -1,7 +1,16 @@
 package kourpa;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
     public class Main {  public static void main( String[] args ) {
+        String jdbcUrl = "jdbc:sqlite:/socialmedia.db";
+        try {
+			Connection conn = DriverManager.getConnection(jdbcUrl);
+        } catch (SQLException e1) {
+			e1.printStackTrace();
+		}
         HashMap<Integer,ArrayList<User>> people = new HashMap<>();
         ArrayList<User> ar = new ArrayList<>();
         User user1 = new User("user1","George2021","george@gmail.com","sports","science","movies");
