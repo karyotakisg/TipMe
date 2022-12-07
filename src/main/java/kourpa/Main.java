7727acf5351dc0bd9a196a4967ae9c1f9b7ce683
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2022-12-07 10:45:21 +0200

    modifying main and pom for SQLite

--- src/main/java/kourpa/Main.java
+++ src/main/java/kourpa/Main.java
@@ -1,8 +1,21 @@
 package kourpa;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
     public class Main {  
         public static void main( String[] args ) {
+            String jdbcUrl = "jdbc:sqlite:/socialmedia.db";
+		
+		try {
+			Connection conn = DriverManager.getConnection(jdbcUrl);
+        } catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
         HashMap<Integer,ArrayList<User>> people = new HashMap<>();
         ArrayList<User> ar = new ArrayList<>();
         User user1 = new User("user1","George2021","george@gmail.com","sports","science","movies");
