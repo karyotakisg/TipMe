48d9e6dac33121e79a2334dc26eb4f2c8675d2a3
Eqerem-Hena <t8210164@aueb.gr>
2022-12-09 23:33:14 +0200

    Change the main

--- src/main/java/kourpa/Main.java
+++ src/main/java/kourpa/Main.java
@@ -9,15 +9,15 @@ public class Main {
 		
 	}
 
-		//IDandPasswords idpas = new IDandPasswords();
-		//LoginPage loginPage = new LoginPage(idpas.getLogininfo());
-	
-		/*HashMap<Integer,ArrayList<User>> people = new HashMap<>();
-        ArrayList<User> ar = new ArrayList<>();
-        User user1 = new User("user1","George2021","george@gmail.com","sports","science","movies");
-        ar.add(user1);
-        people.put(0,ar);
-        MyProfile obj = new MyProfile(user1);*/
+
+
+
+
+
+
+
+
+     
 	
 	}
 
