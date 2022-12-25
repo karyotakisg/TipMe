d9ba4243019990659fc0bd18bc80f93462623808
ApostolosKourpadakis <t8210068@aueb.gr>
2022-12-25 17:37:27 +0200

    Essential class i forgot to upload

---
+++ src/main/java/kourpa/kourpatestclasses/ExploreButtons.java
@@ -0,0 +1,27 @@
+public class ExploreButtons {
+
+	public static String numbers(int n) {
+		if (n == 0) {
+			return "Science";
+		} else if (n == 1) {
+			return "Sports";
+		} else if (n == 2) {
+			return "Music";
+		} else if (n == 3) {
+			return "Fashion";
+		} else if (n == 4) {
+			return "Travel";
+		} else  {
+			return "Fitness & Lifestyle";
+		}
+
+	}
+
+
+}
+
+
+
+
+
+
