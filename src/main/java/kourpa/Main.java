c90808cc624c0b58d3e8b0be21bfd0ab9ad63c57
Dimitris Doukas <t8210038@aueb.gr>
2023-01-03 23:32:35 +0200

    Add testHome testing likes

--- src/main/java/kourpa/Main.java
+++ src/main/java/kourpa/Main.java
@@ -8,13 +8,13 @@ import java.util.HashMap;
 public class Main {
 	public static void main(String[] args) {
 		/*
-		 * HashMap<Integer,ArrayList<User>> people = new HashMap<>(); ArrayList<User> ar
+		 *HashMap<Integer,ArrayList<User>> people = new HashMap<>(); ArrayList<User> ar
 		 * = new ArrayList<>(); User user1 = new
 		 * User("user1","George2021","george@gmail.com","sports","science","movies");
 		 * Menu menu = new Menu(); ar.add(user1); people.put(0,ar);
 		 */
 		Upload upload = new Upload();
-
+		
 
 
 	}
